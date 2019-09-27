d29f6a61dd3c8433097ade0743591b439386e797
Nigel Metheringham <nigel.metheringham@redcentricplc.com>
2019-09-27 16:36:11 +0100

    Sometimes objects are returned without a type field!

--- lib/VMware/vCloudDirector2/API.pm
+++ lib/VMware/vCloudDirector2/API.pm
@@ -162,8 +162,8 @@ method _build_json () { return Cpanel::JSON::XS->new->utf8; }
 
 # ------------------------------------------------------------------------
 method _decode_xml_response ($response) {
-        my $content = $response->decoded_content;
-          return  if ( length($content) == 0 );
+    my $content = $response->decoded_content;
+    return if ( length($content) == 0 );
 
     VMware::vCloudDirector2::Error->throw(
         { message => "Not a XML response as expected - $content", response => $response } )
@@ -184,7 +184,7 @@ method _decode_xml_response ($response) {
 # ------------------------------------------------------------------------
 method _decode_json_response ($response) {
     my $content = $response->decoded_content;
-          return  if ( length($content) == 0 );
+    return if ( length($content) == 0 );
 
     VMware::vCloudDirector2::Error->throw(
         {   message  => "Not a JSON response as expected - $content",
@@ -444,8 +444,13 @@ method _build_returned_objects ($response) {
 
         # See if this is a list of things, in which case the type element will
         # be thingList and it will have a set of thing in it
-        my $mime_type  = $hash->{type};
-        my $type       = ( $mime_type =~ m|^application/vnd\..*\.(\w+)\+json$| ) ? $1 : $mime_type;
+        my $mime_type = $hash->{type};
+        unless ( defined($mime_type) ) {
+            $mime_type = $response->header('Content-Type');
+            $mime_type =~ s/;.*//;
+            $hash->{type} = $mime_type;
+        }
+        my $type = ( $mime_type =~ m|^application/vnd\..*\.(\w+)\+json$| ) ? $1 : $mime_type;
         my $thing_type = ( substr( $type, -4, 4 ) eq 'List' ) ? substr( $type, 0, -4 ) : $type;
 
         if (    ( $type ne $thing_type )
